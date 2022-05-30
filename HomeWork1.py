9da9f183873a7ad1de5845e6b1cb02ea3f1bf668
sempython <105605645+sempython@users.noreply.github.com>
2022-05-29 21:23:23 -0700

    Update HomeWork1.py

--- HomeWork1.py
+++ HomeWork1.py
@@ -1,3 +1,44 @@
+# Dice wars...
+# Player who scores most points win...
+
+# Enter player1 name
+
+# Enter player2 name
+
+# Hi player1 and player2, the game is starting..
+
+# There will be 5 rounds
+
+# player1 is rolling a dice between 1-10
+# player1 rolls: 7
+
+# player2 is rolling a dice between 1-10
+# player2 rolls: 4
+
+# Round 1 is over... player1 has 1 points, player2 has 0 points
+
+# player1 is rolling a dice between 1-10
+# player1 rolls: 3
+
+#w player2 is rolling a dice between 1-10
+# player2 rolls: 5
+
+# Round 2 is over... player1 has 1 points, player2 has 1 points
+
+# player1 is rolling a dice between 1-10
+# player1 rolls: 1
+
+# player2 is rolling a dice between 1-10
+# player2 rolls: 7
+
+# Round 3 is over... player1 has 2 points, player2 has 1 points
+# Game is over...
+# player2 won by 2-1
+
+# OR
+# Game is over...
+# Tie, score is 1-1
+
 PlayerPoints1 = 0
 PlayerPoints2 = 0
 import random
@@ -9,15 +50,15 @@ print ("Player 2 Type your Name")
 Player2 = str(input())
 print ("The Game is Starting")
 while (PlayerPoints1 < 3) and (PlayerPoints2 < 3):
-    Random1 = str(random.randint(1,10))
-    print (Player1 + " has " + Random1 + " Points")
-    Random2 = str(random.randint(1,10))
-    print (Player2 + " has " + Random2 + " Points")
+    Random1 = int(random.randint(1,10))
+    print (Player1 + " has " + str(Random1) + " Points")
+    Random2 = int(random.randint(1,10))
+    print (Player2 + " has " + str(Random2) + " Points")
     if (Random1 > Random2):
-        print (Player1 + " Won the First Round by " + Random1 + " Points")
+        print (Player1 + " Won this Round by " + str(Random1) + " Points")
         PlayerPoints1 = PlayerPoints1 + 1
     if (Random2 > Random1):
-        print (Player2 + " Won the First Round by " + Random2 + " Points")
+        print (Player2 + " Won this Round by " + str(Random2) + " Points")
         PlayerPoints2 = PlayerPoints2 + 1
     if (Random1 == Random2):
         print ("Tie Nobody Wins")  
