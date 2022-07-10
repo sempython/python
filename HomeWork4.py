1d6f7a5a736dcc4c8f7e7b72d31a7a076098731f
sempython <105605645+sempython@users.noreply.github.com>
2022-07-10 12:08:54 -0700

    Create HomeWork4.py

---
+++ HomeWork4.py
@@ -0,0 +1,19 @@
+# hangman with array
+
+# Lets start, whats the secret?
+
+# Ok.. you have 5 guesses left.
+# Enter letter:
+
+# _ _ _ _ _ _ a _ _
+
+# Ok.. you have 4 guesses left
+# _ b _ _ _ _ a _ b
+
+# ...
+# Ok.. no more guesses left, please solve the word..
+# Your guess was wrong. You lost
+
+# OR
+
+# Your guess was correct. You won
