f9738a6e5beeaa3bbabc378af859fac17c6bd045
sempython <105605645+sempython@users.noreply.github.com>
2022-07-10 12:08:19 -0700

    Create HomeWork5.py

---
+++ HomeWork5.py
@@ -0,0 +1,38 @@
+# Library system
+
+# create array of 10 books..
+
+# Welcome. Here are our services..
+#                                     l  
+# it is an menu not in orderrrrrrr    v
+
+# 1. Checkout book
+# 2. Search book
+# 3. Return book
+# 4. View your account
+# 5. Exit 
+
+# Checkout process
+# Enter book name to check out.. BookOne
+# Book not found..
+# OR
+# Book 'BookOne' is checked out.
+# Show main menu again
+
+# Search book..
+# Please enter book name to search... BookOne
+# Sorry, 'bookOne' not found
+# Show main manu again
+
+# return book..
+# Enter book name to return... BookOne
+# Thanks, 'BookOne' is returned
+# OR
+# Sorry you never checked out that book.
+# Show main menu
+
+# View your account
+# Here are the books you have checked out...
+# 1. BookOne
+# 2. BookTwo
+# 3. BookThree
